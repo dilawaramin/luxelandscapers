39462403117414e98c754b65b9911f6c9b277743
Dilawar Amin <79779873+dilawaramin@users.noreply.github.com>
2024-12-01 20:03:21 -0500

    Update Navbar styling and global CSS variables for improved design consistency

--- src/app/mycomponents/Navbar.tsx
+++ src/app/mycomponents/Navbar.tsx
@@ -16,38 +16,41 @@ function Navbar() {
 
     return (
 
-<nav className="flex flex-row justify-between p-5 bg-luxeGreenMain">
-    <div className="m-0 md:pl-6">
-        <img src="/pictures/luxelogo.png" alt="logo" className="h-12 md:h-16 m-0"/>
-    </div>
+<nav className="absolute top-0 left-0 w-full">
+<div className="flex flex-row justify-between align-middle p-5">
+  <div className="m-0 md:pl-6 flex flex-row items-center justify-center w-auto">
+        <img src="/pictures/luxelogo.png" alt="logo" className="h-12 pr-4 md:h-16 m-0"/>
+        <h1 className="text-3xl font-bold p-0 m-0">LUXE</h1>
+  </div>
 
-    <div className="flex flex-row">
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem >
-              <Link href="/"  legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Home
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/about" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  About Us
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/contact" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Contact
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-      </div>
+  <div className="flex flex-row">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem >
+            <Link href="/"  legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Home
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/about"  legacyBehavior passHref>
+              <NavigationMenuLink className= {`${navigationMenuTriggerStyle()}`}>
+                About Us
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/contact" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Contact
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+</div>
 </nav>
 
     )
